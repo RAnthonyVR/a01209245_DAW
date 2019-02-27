b353b0c6a7298026ff3256d437a5447161e9d3a2
Tony <A01209245@itesm.mx>
2019-02-26 21:39:11 -0600

    Terminar lab 11

--- Laboratorio11/lab11_huellaView.php
+++ Laboratorio11/lab11_huellaView.php
@@ -3,12 +3,16 @@
     encabezado(); 
     titulo("Resultados de tu huella de carbono");
 ?>
+
+//Aquí se previene el XSS
+
     <body>
         <p>Tus resultados son:</p>
         <p>
-            Hola <?php echo $nombre ?> ! Tu huella de carbono es de: <strong><?php echo $huella ?></strong> toneladas de CO2 al año.
+            ¡Hola <?php echo htmlspecialchars (htmlentities($_POST["nombre"]), ENT_QUOTES, 'UTF-8') ?>! Tu huella de carbono es de: <strong><?php echo htmlspecialchars ($huella, ENT_QUOTES, 'UTF-8') ?></strong> toneladas de CO2 al año.
         </p>
         <p>La huella media por persona en Estados Unidos es de 16.49 toneladas. La huella de carbono media mundial es de unas 4 toneladas.</p>
+
         <?php 
         if($clasificacion){
             echo "Debido a que tu huella es alta podrias usar focos ahorradores y calentadores solares.";
--- Laboratorio11/registro_controller.php
+++ Laboratorio11/registro_controller.php
@@ -1,30 +1,44 @@
 <?php
-    function calcularHuella($elect, $gaslp, $gasprop) {
-        $huella=($elect*181*0.12283503255128)+($gaslp*2.96)+($gasprop*2.94);
-        $huella=$huella/1000; //Toneladas de CO2
-        return $huella;
-    }
 
-    function clasificacion($huella) {
-        if($huella>4){
-            return true;
-        }else{
-            return false;
-        }             
-    }
+function calcularHuella($elect, $gaslp, $gasprop) {
+    $huella=($elect*181*0.12283503255128)+($gaslp*2.96)+($gasprop*2.94);
+    $huella=$huella/1000; //Toneladas de CO2
+    return $huella;
+}
 
-    $nombre=$_POST['nombre'];
-    $correo=$_POST['correo'];
+function clasificacion($huella) {
+    if($huella>4){
+        return true;
+    }else{
+        return false;
+    }             
+}
 
-    if (isSet($_POST['nombre']) && isSet($_POST['correo'])&& isSet($_POST['elect'])&& isSet($_POST['glp'])&& isSet($_POST['gp'])) {
+if(isSet($_POST['calcular'])){
+    $_POST["nombre"] = htmlentities($_POST["nombre"]);
+    $_POST["correo"] = htmlentities($_POST["correo"]);
+    $_POST["elect"] = htmlentities($_POST["elect"]);
+    if (isSet($_POST['nombre']) && isSet($_POST['correo'])&& isSet($_POST['elect'])&& isSet($_POST['glp'])&& isSet($_POST['gp'])  && $_POST["elect"] != "" && $_POST["glp"] != ""
+           && $_POST["gp"] != "" ) {
         $huella = calcularHuella($_POST['elect'], $_POST['glp'],$_POST['gp']);
         $clasificacion = clasificacion ($huella);
         include('lab11_huellaView.php');
+
+        if (isSet($_GET['huella'])) {
+            $huella = $_GET['huella'];
+            $clasificacion = clasificacion ($huella);
+            include('lab11_huellaView.php');
+        }
+    }else{
+        include("partials/_head.html");
+        $error = "Los datos tienen errores";
+        include("partials/_index.html");
+        include("partials/_footer.html");
     }
-        
-    if (isSet($_GET['huella'])&&isSet($_POST['nombre']) && isSet($_POST['correo'])&& isSet($_POST['elect'])&& isSet($_POST['glp'])&& isSet($_POST['gp'])) {
-        $huella = $_GET['huella'];
-        $clasificacion = clasificacion ($huella);
-        include('lab11_huellaView.php');
-    }
+}else{
+    include("partials/_head.html");
+    include("partials/_index.html");
+    include("partials/_footer.html");
+}
+
 ?>
