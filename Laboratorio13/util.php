e195174226e5bc208a0b87b9f3e197b1b6472e65
Tony <A01209245@itesm.mx>
2019-03-01 22:05:59 -0600

    Responder preguntas lab 13

---
+++ Laboratorio13/util.php
@@ -0,0 +1,27 @@
+<?php
+    
+    function encabezado() {
+        include("partials/_head.html");
+    }
+
+    function titulo($titulo) {
+        echo "<h1>$titulo</h1>";
+    }
+
+    function regresarPagina() {
+        echo "<a href='index.php'>Volver al index</a>";
+    }
+
+    function indice() {
+        include("partials/_index.html");
+    }
+    
+     function footer() {
+        include("partials/_footer.html");
+    }
+    function inicio_de_sesion(){
+        include("partials/_login.html");
+    }
+    
+
+?>
