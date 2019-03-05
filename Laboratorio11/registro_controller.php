30a72d27f64f7f2771b5f4c85219ba7f57dd5342
Tony <A01209245@itesm.mx>
2019-03-05 11:23:01 -0600

    Corregir comentarios de Gleason

--- Laboratorio11/registro_controller.php
+++ Laboratorio11/registro_controller.php
@@ -1,13 +1,10 @@
 <?php
 
-<<<<<<< HEAD
 function calcularHuella($elect, $gaslp, $gasprop) {
     $huella=($elect*181*0.12283503255128)+($gaslp*2.96)+($gasprop*2.94);
     $huella=$huella/1000; //Toneladas de CO2
     return $huella;
 }
-=======
-   
 
     function clasificacion($huella) {
         if($huella>4){
@@ -16,7 +13,6 @@ function calcularHuella($elect, $gaslp, $gasprop) {
             return false;
         }             
     }
->>>>>>> 5d2c69d0489fe4dbba1010dac7ad70d544d44b4a
 
 function clasificacion($huella) {
     if($huella>4){
@@ -26,17 +22,17 @@ function clasificacion($huella) {
     }             
 }
 
-if(isSet($_POST['calcular'])){
+if(isset($_POST['calcular'])){
     $_POST["nombre"] = htmlentities($_POST["nombre"]);
     $_POST["correo"] = htmlentities($_POST["correo"]);
     $_POST["elect"] = htmlentities($_POST["elect"]);
-    if (isSet($_POST['nombre']) && isSet($_POST['correo'])&& isSet($_POST['elect'])&& isSet($_POST['glp'])&& isSet($_POST['gp'])  && $_POST["elect"] != "" && $_POST["glp"] != ""
+    if (isset($_POST['nombre']) && isset($_POST['correo'])&& isset($_POST['elect'])&& isset($_POST['glp'])&& isset($_POST['gp'])  && $_POST["elect"] != "" && $_POST["glp"] != ""
            && $_POST["gp"] != "" ) {
         $huella = calcularHuella($_POST['elect'], $_POST['glp'],$_POST['gp']);
         $clasificacion = clasificacion ($huella);
         include('lab11_huellaView.php');
 
-        if (isSet($_GET['huella'])) {
+        if (isset($_GET['huella'])) {
             $huella = $_GET['huella'];
             $clasificacion = clasificacion ($huella);
             include('lab11_huellaView.php');
@@ -47,19 +43,14 @@ if(isSet($_POST['calcular'])){
         include("partials/_index.html");
         include("partials/_footer.html");
     }
-<<<<<<< HEAD
 }else{
     include("partials/_head.html");
     include("partials/_index.html");
     include("partials/_footer.html");
-}
-
-=======
-        
-    if (isSet($_GET['huella'])) {
+}       
+    if (isset($_GET['huella'])) {
         $huella = $_GET['huella'];
         $clasificacion = clasificacion ($huella);
         include('lab11_huellaView.php');
     }
->>>>>>> 5d2c69d0489fe4dbba1010dac7ad70d544d44b4a
 ?>
