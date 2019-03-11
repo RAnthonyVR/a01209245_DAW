bfbeda93ec4f1203d2bf254ceeb5a349db5d8daa
Tony <A01209245@itesm.mx>
2019-03-10 19:58:52 -0600

    Terminar lab16

---
+++ Laboratorio16/agregar_fruta.php
@@ -0,0 +1,12 @@
+<?php
+    //session_start();
+    require_once("util.php");
+    include("partials/_head.html");
+    /*
+    if (isset($_SESSION["error"])) {
+        echo $_SESSION["error"];
+        unset($_SESSION["error"]);
+    }*/
+    include("partials/_formInsertar.html");
+    include("partials/_footer.html");    
+?>
