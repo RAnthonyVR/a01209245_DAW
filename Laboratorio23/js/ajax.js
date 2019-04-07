09b1d9090acd631cb51ca20e06b46ad638b91aae
Tony <A01209245@itesm.mx>
2019-04-07 13:16:42 -0500

    Terminar lab23

---
+++ Laboratorio23/js/ajax.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    /*
+    $('#submit').click(function(){
+        $.get( "../controlador/enviar.php", { subject: document.getElementById('subject').value, text: document.getElementById('text').value })
+          .success(function( data ) {
+              alert("Su mensaje ha sido enviado");
+              
+              //document.getElementById("subject").value="";
+              //document.getElementById("text").value="";
+              })
+              
+            .fail(function( data ) {
+               alert("Su mensaje no pudo ser enviado");
+              //document.getElementById("subject").value="";
+              //document.getElementById("text").value="";
+                
+            });
+    });*/
+});
